b0c84f307b0d8a9c1bcbdfdc0b0df9b22bc6b249
Ross <Ross@myyearofcode.com>
2019-06-10 15:35:52 -0400

    adding /account route

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show]
+      resources :users, only: [:index, :show, :update]
     end
   end
   
+  get '/account', to: 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
