e1d9d09f9c9f49a0dcf4ee43c638843ccc194197
Ross <Ross@myyearofcode.com>
2019-06-16 22:34:50 -0400

    sketch for graph structure + methods

---
+++ app/models/graph.rb
@@ -0,0 +1,14 @@
+class graph
+    def initialize (graph)
+        @dirtyGraph = dirtyGraph
+        @graph = []
+    end
+    # this is a data structure consisting of days (date:commits key:val pair)
+    # do we pass in the svg and then parse it in here? 
+    # with a print method
+
+    # we will need an accessor method for specific dates. 
+    # we could also build calculating the stats into this class.
+
+
+end
